545459597d8785d9324993bd11171db276b9a229
Chris Clarke <332873+CheeseTheMonkey@users.noreply.github.com>
2020-12-01 10:56:47 +0000

    2020 - day01

---
+++ 2020/day01.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+from operator import mul
+
+def find_product(numbers, result=2020, item_count=2, output_string="Part 1: "):
+    for a in combinations(l, item_count):
+        if sum(a) == result:
+            print output_string, reduce(mul, a, 1)
+            break
+
+l = [int(i.strip()) for i in open("day01.input")]
+
+find_product(l)
+find_product(l, item_count=3, output_string="Part 2: ")
