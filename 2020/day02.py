c2458354964eae462356db5fa7bef814d3f268b2
Chris Clarke <332873+CheeseTheMonkey@users.noreply.github.com>
2020-12-02 08:50:03 +0000

    2020 day2

---
+++ 2020/day02.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+def get_password_tuple(line):
+    bounds, letter, password = line.split()
+    lower, upper = [int(a) for a in bounds.split('-')]
+    letter = letter.strip(':')
+    return (lower, upper, letter, password)
+
+
+passwords = [get_password_tuple(l.strip()) for l in open("day02.input").readlines()]
+p1_valid_count = 0
+p2_valid_count = 0
+
+for lower, upper, letter, password in passwords:
+    counts = Counter(password)
+    if lower <= counts[letter] <= upper:
+        p1_valid_count += 1
+    if (password[lower - 1] == letter) ^ (password[upper - 1] == letter):
+        p2_valid_count += 1
+
+print "Part 1: ", p1_valid_count
+print "Part 2: ", p2_valid_count
+
