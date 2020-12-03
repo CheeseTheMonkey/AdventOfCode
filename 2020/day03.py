d73f8821950f2975e7ec091d94fd4727927623bd
Chris Clarke <332873+CheeseTheMonkey@users.noreply.github.com>
2020-12-03 08:52:09 +0000

    2020 day 3

---
+++ 2020/day03.py
@@ -0,0 +1,29 @@
+from operator import mul
+
+tree_map = open("day03.input").read().split()
+
+
+def get_collisions(x=1, y=1):
+    loc_x = 0
+    loc_y = 0
+    max_x = len(tree_map[0])
+
+    tree_count = 0
+
+    while loc_y < len(tree_map):
+        if tree_map[loc_y][loc_x] == '#':
+            tree_count += 1
+        loc_y += y
+        loc_x = (loc_x + x) % max_x
+
+    print tree_count
+    return tree_count
+
+print "Part 1: ", get_collisions(x=3)
+
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+collisions = [get_collisions(*coords) for coords in slopes]
+print collisions
+
+
+print "Part 2: ", reduce(mul, collisions, 1)
