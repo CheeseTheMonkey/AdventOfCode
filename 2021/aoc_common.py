b6ed100d1fd8d9c2ced481cdecc9eef46ee97d1e
Chris Clarke <CheeseTheMonkey@users.noreply.github.com>
2021-12-05 09:23:14 +0000

    2021 day 4

--- 2021/aoc_common.py
+++ 2021/aoc_common.py
@@ -1,5 +1,9 @@
+
+def read_file(filename):
+    return open(filename).read().rstrip('\n')
+
 def read_file_by_line(filename):
-    return open(filename).read().rstrip('\n').split('\n')
+    return read_file(filename).split('\n')
 
 def read_file_by_int(filename):
     return [int(a) for a in read_file_by_line(filename)]
